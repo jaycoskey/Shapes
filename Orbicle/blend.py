578c229118a29d7f67a64a658a92b89ea29b3845
jaycoskey <jay.coskey@gmail.com>
2016-05-24 23:48:25 -0700

    Improve handling of addition of materials

--- Orbicle/blend.py
+++ Orbicle/blend.py
@@ -1,7 +1,9 @@
 #!/bin/python
 
-# import os
-# import sys
+# TODO: Learn more about the implementation details of Blender materials and object selection.
+# TODO: Implement setting of materials.
+
+import sys
 
 from itertools import chain
 
@@ -24,7 +26,7 @@ from orbicle import *
 
 # Spheres (bpy.ops.mesh.primitive_ico_sphere_add):
 #     https://www.blender.org/api/blender_python_api_2_57_release/bpy.ops.mesh.html
-def blender_add_point(r, point):
+def blender_add_point(name, mat, r, point):
     bpy.ops.mesh.primitive_ico_sphere_add(
         subdivisions=3
         , size=r
@@ -34,6 +36,8 @@ def blender_add_point(r, point):
         # rotation=(0.0, 0.0, 0.0)  # Not needed
         # layers
         )
+    # TODO: Once properly implemented, move these settings elsewhere.
+    name_material_set(bpy.context.object, name, mat)
 
 # Cylinders (bpy.ops.mesh.primitive_cylinder_add):
 #     https://www.blender.org/api/blender_python_api_2_57_release/bpy.ops.mesh.html
@@ -45,7 +49,7 @@ def blender_add_point(r, point):
 #         bpy.context.object.data.bevel_object = bpy.data.objects[obj.name] #a cylinder
 #     Documentation on primitive_bezier_circle_add can be found at:
 #         https://www.blender.org/api/blender_python_api_2_59_2/bpy.ops.curve.html
-def blender_add_cylinder(cylinder):
+def blender_add_cylinder(name, mat, cylinder):
     loc          = average_point([cylinder.center1, cylinder.center2])
     zunit        = Vector(0, 0, 1)
     cylinder_dir = 0.5 * (cylinder.center2 - cylinder.center1)
@@ -61,10 +65,12 @@ def blender_add_cylinder(cylinder):
         , rotation=(euler_rot.x, euler_rot.y, euler_rot.z)
         # layers
         )
+    # TODO: Once properly implemented, move these settings elsewhere.
+    name_material_set(bpy.context.object, name, mat)
 
 # Tori (bpy.ops.mesh.primitive_torus_add):
 #     https://www.blender.org/api/blender_python_api_2_57_release/bpy.ops.mesh.html
-def blender_add_torus(torus):
+def blender_add_torus(name, mat, torus):
     zunit   = Vector(0, 0, 1)
     euler_rot = vectors2euler(zunit, torus.normal)
     bpy.ops.mesh.primitive_torus_add(
@@ -79,6 +85,8 @@ def blender_add_torus(torus):
         , location=(torus.center.x, torus.center.y, torus.center.z)
         , rotation=(euler_rot.x, euler_rot.y, euler_rot.z)
         )
+    # TODO: Once properly implemented, move these settings elsewhere.
+    name_material_set(bpy.context.object, name, mat)
 
 def blender_write_outfile(filepath):
     bpy.ops.export_mesh.stl(
@@ -112,10 +120,46 @@ def material_make(name, diffuse, specular, alpha):
     mat.ambient            = 1
     return mat
 
-# See "setMaterial" at the URL above.
-def material_set(ob, mat):
-    me = ob.data
+def material_set(obj, mat):
+    bpy.ops.object.mode_set(mode='OBJECT')
+    for mat_slot in obj.material_slots:
+        bpy.ops.object.material_slot_remove()
+    obj.data.materials.append(mat)
+
+def material_set_by_name(name, mat):
+    # bpy.ops.object.editmode_toggle()
+    bpy.ops.object.select_all(action='DESELECT')
+
+    # bpy.ops.objects.select_pattern(pattern=name).select_all(action='SELECT')
+    # How does the following line treat multiple objects with the same name?
+    grp = bpy.data.groups.new(name)
+    obj = bpy.data.objects.get(name)
+    if (obj):
+        print('Found object(s) with name: ' + name)
+        obj.select = True
+        bpy.context.scene.objects.active = obj
+        # TODO: Linking to group should only appear in selected_objects loop.  Duplicating here for testing.
+        # grp.objects.link(obj)
+        material_set(obj, mat)
+    else:
+        print('Did not find object with name: ' + name)
+
+    for obj in bpy.context.selected_objects:
+        grp.objects.link(obj)
+        material_set(obj, mat)
+    # bpy.ops.object.editmode_toggle()
+
+def name_material_set(obj, name, mat):
+    obj.name = name
+    me = obj.data
+    me.materials.clear()
     me.materials.append(mat)
+
+def progress(done=False):
+    if done:
+        print('')
+    else:
+        print('.', end='')
 
 def vectors2euler(vec_ref, vec_actual):
     rot_axis  = vec_ref.cross(vec_actual).normalized()
@@ -131,53 +175,49 @@ def main():
     delete_all()
     orb = Orbicle()
 
-    mat_red   = material_make('Red',   (1,0,0), (1,0,0), 1)
-    mat_white = material_make('White', (1,1,1), (1,1,1), 1)
-    mat_blue  = material_make('Blue',  (0,0,1), (0,0,1), 1)
+    mat_red     = material_make('Red',     (1,0,0), (1,0,0), 1)
+    mat_green   = material_make('Green',   (0,1,0), (0,1,0), 1)
+    mat_blue    = material_make('Blue',    (0,0,1), (0,0,1), 1)
 
-    for obj in bpy.data.objects:  # vs. bpy.context.scene.objects
-        if not obj.data.materials:  # obj.type == "MESH"
-            print('Setting object material to red')
-            bpy.ops.object.mode_set(mode='OBJECT')
-            for mat_slot in obj.material_slots:
-                mat = mat_slot.material
-                mat.user_clear()
-                bpy.data.materials.remove(mat)
+    mat_yellow  = material_make('Yellow',  (1,1,0), (1,1,0), 1)
+    mat_magenta = material_make('Magenta', (1,0,1), (1,0,1), 1)
+    mat_cyan    = material_make('Cyan',    (0,1,1), (0,1,1), 1)
+
+    mat_pink    = material_make('Pink',    (1,     0.7529,0.7961), (1,     0.7529,0.7961), 1)
+    mat_purple  = material_make('Purple',  (0.6275,0.1254,0.9412), (0.6275,0.1254,0.9412), 1)
+    mat_white   = material_make('White',   (1,1,1), (1,1,1), 1)
+
+    sys.stdout.flush()
+
     for t in orb.tori12:
-        blender_add_torus(t)
-        # material_set(bpy.context.object, red)
-        # bpy.ops.object.mode_set(mode='OBJECT')
-        for obj in bpy.data.objects:
-            # material_set(bpy.context.object, mat_blue)
-            obj.data.materials.append(mat_red)
-            if len(obj.data.materials) > 0:
-                obj.data.materials[0] = mat_red
-            else:
-                obj.data.materials.append(mat_red)
-
+        blender_add_torus('tori12',          mat_blue, t);     progress()
     for t in orb.tori20:
-        blender_add_torus(t)
-        material_set(bpy.context.object, mat_blue)
+        blender_add_torus('tori20',          mat_green, t);    progress()
     for hv in orb.hexgrid_verts:
-        blender_add_point(hexgrid_vertex_radius, hv)
-        material_set(bpy.context.object, mat_white)
+        r = hexgrid_vertex_radius
+        blender_add_point('hexgrid_vert',    mat_red, r, hv);  progress()
     for he in orb.hexgrid_edges:
-        blender_add_cylinder(he)
-        material_set(bpy.context.object, mat_white)
+        blender_add_cylinder('hexgrid_edge', mat_pink, he);    progress()
+    progress(done=True)
+    material_set_by_name('tori12',       mat_blue)
+    material_set_by_name('tori20',       mat_green)
+    material_set_by_name('hexgrid_edge', mat_pink)
+    material_set_by_name('hexgrid_vert', mat_red)
+
     blender_write_outfile('orbicle.stl')
+
+########################################
+# MAIN
+########################################
+if __name__ == '__main__':
+    main()
+    # test_cube_facetori()
+    # test_cube_wireframe()
+    # test_icosa_wireframe()
 
 ########################################
 # TEST CODE
 ########################################
-def test_cube_wireframe():
-    delete_all()
-    cube = Cube()
-    for v in cube.verts:
-        blender_add_point(test_cube_vertex_radius, v)
-    for e in cube.edges:
-        blender_add_cylinder(e)
-    blender_write_outfile('test_cube_wireframe.stl')
-
 def test_cube_facetori():
     delete_all()
     cube = Cube()
@@ -187,6 +227,15 @@ def test_cube_facetori():
         t = Torus(test_cube_edge_radius, f)
         blender_add_torus(t)
     blender_write_outfile('test_cube_facetori.stl')
+
+def test_cube_wireframe():
+    delete_all()
+    cube = Cube()
+    for v in cube.verts:
+        blender_add_point(test_cube_vertex_radius, v)
+    for e in cube.edges:
+        blender_add_cylinder(e)
+    blender_write_outfile('test_cube_wireframe.stl')
 
 def test_icosa_wireframe():
     delete_all()
@@ -207,10 +256,3 @@ def test_icosa_wireframe():
     for e in icosa_edges:
         blender_add_cylinder(e)
     blender_write_outfile('test_icosa_wireframe.stl')
-
-if __name__ == '__main__':
-    main()
-    test_cube_facetori()
-    test_cube_wireframe()
-    test_icosa_wireframe()
-
